e42221fc2b635a8b286ffdabb6a03ef52274ee50
omar <omaraziz.dev@proton.me>
2025-01-23 22:26:53 -0800

    feat: ucs03

--- script/Transfer.s.sol
+++ script/Transfer.s.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
 import {Script, console} from "forge-std/Script.sol";
@@ -6,18 +5,18 @@ import {Transfer} from "../src/Transfer.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 contract TransferScript is Script {
-  address public constant USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
+    address public constant USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
 
-  function run() public {
-    uint256 privateKey = vm.envUint("PRIVATE_KEY");
-    vm.startBroadcast(privateKey);
-    Transfer transfer = new Transfer();
+    function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+        Transfer transfer = new Transfer();
 
-    IERC20(USDC).transfer(address(transfer), 1500000);
+        IERC20(USDC).transfer(address(transfer), 1500);
 
-    console.log("transferring");
-    transfer.transferAsset();
-    console.log("complete");
-    vm.stopBroadcast();
-  }
+        console.log("transferring");
+        transfer.transferAsset();
+        console.log("complete");
+        vm.stopBroadcast();
+    }
 }
--- src/Transfer.sol
+++ src/Transfer.sol
@@ -1,48 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
-struct LocalToken {
-  address denom;
-  uint128 amount;
-}
-
-struct Height {
-  uint64 revisionNumber;
-  uint64 revisionHeight;
-}
-
-interface IRelay {
-  function send(
-    string calldata sourceChannel,
-    bytes calldata receiver,
-    LocalToken[] calldata tokens,
-    string calldata extension,
-    Height calldata timeoutHeight,
-    uint64 timeoutTimestamp
+interface Ucs03 {
+  function transfer(
+      uint32 channelId,
+      bytes calldata receiver,
+      address baseToken,
+      uint256 baseAmount,
+      bytes calldata quoteToken,
+      uint256 quoteAmount,
+      uint64 timeoutHeight,
+      uint64 timeoutTimestamp,
+      bytes32 salt
   ) external;
 }
 
 contract Transfer {
-  // https://github.com/unionlabs/union/blob/main/evm/README.md#sepolia
   address public constant relay = 0xD0081080Ae8493cf7340458Eaf4412030df5FEEb;
+  address public constant usdc = 0xa833B03D8ED1228C4791cBfAb22b3ED57954429F;
+  address public constant receiver = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
 
-  // https://github.com/unionlabs/union/blob/main/evm/contracts/apps/ucs/01-relay/Relay.sol#L54-L61
   function transferAsset() public {
-    LocalToken[] memory tokens = new LocalToken[](1);
-    tokens[0].denom = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
-    tokens[0].amount = 1000000;
+    IERC20(usdc).approve(relay, 1000000);
 
-    IERC20(tokens[0].denom).approve(relay, 1000000);
-
-    IRelay(relay).send(
-      "channel-90",
+    Ucs03(relay).transfer(
+      90,
       hex"a833B03D8ED1228C4791cBfAb22b3ED57954429F",
-      tokens,
-      "",
-      Height({revisionNumber: 100, revisionHeight: 1000000}),
-      0
+      receiver,
+      1000000,
+      "usdc",
+      1000000,
+      1000000,
+      0,
+      bytes32("")
     );
   }
 }
